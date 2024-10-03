e94cfd38fc0dfa375c64d1bfc876b52a21bb6e86
CzajnikXD <264275@student.pwr.edu.pl>
2024-10-03 14:26:04 +0200

    Upload wersji publicznej

---
+++ MediaCorner/MediaCornerWPF/Lib/API/Calls/MovieController.cs
@@ -0,0 +1,181 @@
+﻿using MediaCornerWPF.Lib.API.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MediaCornerWPF.Lib.API.Calls
+{
+    internal class MovieController
+    {
+        /// <summary>
+        /// Funkcja pobierająca listę popularnych filmów z The Movie Database API.
+        /// </summary>
+        /// <returns>
+        ///     Lista obiektów MovieModel reprezentujących popularne filmy.
+        /// </returns>
+        /// <exception cref="Exception">
+        ///     Rzuca wyjątek w przypadku nieudanej próby pobrania danych.
+        /// </exception>
+        public static async Task<List<MovieModel>> GetPopular()
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"),
+                Headers =
+        {
+            { "accept", "application/json" },
+            { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
+        },
+            };
+
+            using (var response = await ApiController.ApiClient.SendAsync(request))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    var movielist = JObject.Parse(result)["results"].ToString();
+
+                    List<MovieModel> ret = JsonConvert.DeserializeObject<List<MovieModel>>(movielist);
+
+                    return ret;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Funkcja pobierająca listę popularnych filmów z The Movie Database API z podziałem na strony.
+        /// </summary>
+        /// <param name="page">
+        ///     Numer strony do pobrania.
+        /// </param>
+        /// <returns>
+        ///     Lista obiektów MovieModel reprezentujących popularne filmy z wybranej strony.
+        /// </returns>
+        /// <exception cref="Exception">
+        ///     Rzuca wyjątek w przypadku nieudanej próby pobrania danych.
+        /// </exception>
+        public static async Task<List<MovieModel>> GetPopularPaged(int page)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://api.themoviedb.org/3/movie/popular?language=en-US&page={page}"),
+                Headers =
+        {
+            { "accept", "application/json" },
+            { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
+        },
+            };
+
+            using (var response = await ApiController.ApiClient.SendAsync(request))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    var movielist = JObject.Parse(result)["results"].ToString();
+
+                    List<MovieModel> ret = JsonConvert.DeserializeObject<List<MovieModel>>(movielist);
+
+                    return ret;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Funkcja pobierająca szczegóły filmu na podstawie jego identyfikatora z The Movie Database API.
+        /// </summary>
+        /// <param name="id">
+        ///     Identyfikator filmu, którego szczegóły mają zostać pobrane.
+        /// </param>
+        /// <returns>
+        ///     Obiekt MovieModel reprezentujący szczegóły filmu.
+        /// </returns>
+        /// <exception cref="Exception">
+        ///     Rzuca wyjątek w przypadku nieudanej próby pobrania danych.
+        /// </exception>
+        public static async Task<MovieModel> GetMovie(int id)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://api.themoviedb.org/3/movie/{id}?language=en-US"),
+                Headers =
+                {
+                    { "accept", "application/json" },
+                    { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
+                },
+            };
+
+            using (var response = await ApiController.ApiClient.SendAsync(request))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    MovieModel ret = JObject.Parse(result).ToObject<MovieModel>();
+
+                    return ret;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Funkcja wyszukująca filmy na podstawie podanego zapytania w The Movie Database API.
+        /// </summary>
+        /// <param name="query">
+        ///     Zapytanie wyszukiwane.
+        /// </param>
+        /// <returns>
+        ///     Lista obiektów MovieModel reprezentujących wyniki wyszukiwania.
+        /// </returns>
+        /// <exception cref="Exception">
+        ///     Rzuca wyjątek w przypadku nieudanej próby pobrania danych.
+        /// </exception>
+        public static async Task<List<MovieModel>> SearchMovie(string query)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://api.themoviedb.org/3/search/movie?query={query}&include_adult=false&language=en-US&page=1"),
+                Headers =
+                {
+                    { "accept", "application/json" },
+                    { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
+                },
+            };
+
+            using (var response = await ApiController.ApiClient.SendAsync(request))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    var movielist = JObject.Parse(result)["results"].ToString();
+
+                    List<MovieModel> ret = JsonConvert.DeserializeObject<List<MovieModel>>(movielist);
+
+                    return ret;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# **Aplikacja do pracy inżynierskiej służąca do zarządzania obejrzanymi filmami**
+
+## **V1:**
+Zmiany:
+- Dodano okno i logike rejestracji nowego użytkownika
+- Dodano zakładki Currently Popular i jej logikę oraz Friends Activity do zakładki Dashboard
+- Dodano popup wylogowywania i jego logikę przy wciśnięciu guzika przy nazwie użytkownika
+- Poprawiono stylistykę zakładki Your Watchlist oraz Movies
+- Dodano nową zakładkę Friends
+- Dodano ikonę aplikacji
+- Przerobiona funkcjonalość zakładki Movies umożliwiającą przechodzenie po następnych stronach popularnych aktualnie filmów
+
+## **V2:**
+Zmiany:
+- Dodano stylizowane okienka messagebox
+- Przebudowano system users oraz friends
+- Zaimplementowano informacje o dodawaniu i usuwaniu przyjaciół
+- W okienku friends można scrollować informacje o znajomych z możliwością ich usunięcia
+- W okienku users dodano możliwość scrollowania i wyszukiwania użytkowników z możliwością ich dodania do znajomych
+- W okienku settings dodano możliwość zmiany nazwy użytkownika i hasła z odpowiednią logiką blokowania zmian jeżeli nazwa jest pusta lub aktualnie wykorzystywana
+- W okienku dashboard dodano informacje o 20 aktualnie popularnych filmach
+- Małe poprawki w okienkach dla wygody użytkownika
+
+## **V3:**
+Zmiany:
+- Zaimplementowano wyświetlanie aktywności przyjaciół w dashboardzie 
+- Usunięto możliwość dodawania aktualnie zalogowanego użytkownika jako przyjaciela
+- Lekka optymalizacja czasów ładowania i pobierania informacji z bazy danych
+- Drobne poprawki wizualne we wszystkich oknach dla poprawy wygody użytkownika
+### **V3.1:**
+Zmiany:
+- Zaimplementowano możliwość zmiany zdjęcia profilowego użytkownika
+- Poprawiono blokowanie przycisków zmiany hasła i nazwy użytkownika aby poprawnie blokowało po pierwszej zmianie
+### **V3.2:**
+Zmiany:
+- Ukrycie klucza bazy MongoDB
+- Oczyszczenie kodu ze zbędnych komentarzy i dodanie brakujących
